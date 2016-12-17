d694b73ac7efe276d848af37098bd6b1ca499c75
NM Chien <amemberofuit@gmail.com>
2016-12-17 18:07:20 +0700

    [ADD] Loyalty module

---
+++ loyalty_card/__openerp__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Project Bluewave installer',
+    'version': '1.0',
+    'category': 'Trobz Standard Modules',
+    'description': """
+This module will install all module dependencies of Bluewave.
+    """,
+    'author': 'Trobz',
+    'website': 'http://www.trobz.com',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        # ============================================================
+        # SECURITY SETTING - GROUP - PROFILE
+        # ============================================================
+        # 'security/',
+
+        # ============================================================
+        # DATA
+        # ============================================================
+        # 'data/',
+
+        # ============================================================
+        # VIEWS
+        # ============================================================
+        # 'view/',
+
+        # ============================================================
+        # MENU
+        # ============================================================
+        # 'menu/',
+
+        # ============================================================
+        # FUNCTION USED TO UPDATE DATA LIKE POST OBJECT
+        # ============================================================
+        # "data/bluewave_update_functions_data.xml",
+    ],
+
+    'test': [],
+    'demo': [],
+
+    'installable': True,
+    'active': False,
+    'application': True,
+}
